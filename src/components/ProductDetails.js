3a79976f50e0fea58a6877fad39c1054a8c59418
Sabb <sarafaabbas@gmail.com>
2024-07-07 17:38:01 +0100

    second page done

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -7,6 +7,10 @@ import Laptop from "../assets/headphone.png";
 import Heart from "../assets/Heart 1.png";
 import AddCart from "../assets/Add shopping cart.png";
 import ArrowDown from "../assets/Chevron down large.png";
+import Rating from "../assets/rating.png";
+import Cart from "../assets/cartButton.png";
+import BestSeller from "../assets/bestSeller.png";
+import Slider from "../assets/Group 6.png";
 import "../css files/productDetails.css";
 
 function ProductDetails() {
@@ -69,6 +73,78 @@ function ProductDetails() {
                         </div>
                     </div>
                 </article>
+            </section>
+            <section className="relatedProductsSection">
+                <h1>Related Products</h1>
+                <div className="relatedProductsContainer">
+                    <div className="individualRelatedProductContainer" key={id}>
+                        <div className="productImageContainer">
+                            <img className="productImage" src={Laptop} alt="product item" />
+                            <img className="productImageTag" src={BestSeller} alt="best seller tag" />
+                        </div>
+                        <div className="productDetails">
+                            <div className="productTitle">
+                                <h3>Headphone</h3>
+                                <p>product description</p>
+                            </div>
+                            <div className="productPriceNCart">
+                                <div className="productPrice">
+                                    <h2>$32</h2>
+                                    <p>$43</p>
+                                </div>
+                                <img src={Cart} alt="cart button" />
+                            </div>
+                            <div className="productTag">
+                                <img src={Rating} alt="product rating" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="individualRelatedProductContainer" key={id}>
+                        <div className="productImageContainer">
+                            <img className="productImage" src={Laptop} alt="product item" />
+                            <img className="productImageTag" src={BestSeller} alt="best seller tag" />
+                        </div>
+                        <div className="productDetails">
+                            <div className="productTitle">
+                                <h3>Headphone</h3>
+                                <p>product description</p>
+                            </div>
+                            <div className="productPriceNCart">
+                                <div className="productPrice">
+                                    <h2>$32</h2>
+                                    <p>$43</p>
+                                </div>
+                                <img src={Cart} alt="cart button" />
+                            </div>
+                            <div className="productTag">
+                                <img src={Rating} alt="product rating" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="individualRelatedProductContainer" key={id}>
+                        <div className="productImageContainer">
+                            <img className="productImage" src={Laptop} alt="product item" />
+                            <img className="productImageTag" src={BestSeller} alt="best seller tag" />
+                        </div>
+                        <div className="productDetails">
+                            <div className="productTitle">
+                                <h3>Headphone</h3>
+                                <p>product description</p>
+                            </div>
+                            <div className="productPriceNCart">
+                                <div className="productPrice">
+                                    <h2>$32</h2>
+                                    <p>$43</p>
+                                </div>
+                                <img src={Cart} alt="cart button" />
+                            </div>
+                            <div className="productTag">
+                                <img src={Rating} alt="product rating" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img src={Slider} alt="slider" />
             </section>
             <Footer />
         </main>
